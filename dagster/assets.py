3e520677696bc2c419f3e14ed7931bb88ba22c19
Rodrigo Franco <francorodrigognr@yahoo.com.br>
2026-02-27 20:44:13 -0300

    adding feature in the assets

--- dagster/assets.py
+++ dagster/assets.py
@@ -1,9 +1,15 @@
-from dagster import asset
+from dagster import asset, DailyPartitionsDefinition
+from datetime import datetime
 import subprocess
 
-@asset
-def etl_asset():
+daily_partitions = DailyPartitionsDefinition(
+    start_date="2026-02-20"
+)
+
+@asset(partitions_def=daily_partitions)
+def etl_asset(context):
+    partition_date = context.partition_key
     subprocess.run(
-        ["python", "/app/etl/etl.py", "2026-02-27"],
+        ["python", "/app/etl/etl.py", partition_date],
         check=True
     )
